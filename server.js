e8414dbc1cca2831af8178abcb9d1141531082fc
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-03-21 11:37:22 -0600

    Added Procfile

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// for local development
+require('dotenv').config();
+const connectionString = process.env.DATABASE_URL;
+
 const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
