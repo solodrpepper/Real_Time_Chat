a32e77e9498c6e0d91c2b143548a4d7a9b48fdc2
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-03-25 10:04:02 -0600

    Fixed connection stuff

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
-const express = require('express');
+// Grab all my dependencies
+// express
+const express = require("express");
 const app = express();
 
+// set up server
+const server = require("http").createServer(app);
+
+// socket.io
+const io = require("socket.io").listen(server);
+
 // for local development
-require('dotenv').config();
+require("dotenv").config();
 const connectionString = process.env.DATABASE_URL;
 
+// MOST LIKELY WILL NEED TO MOVE TO MODEL
+users = [];
+connections = [];
+
+// set the port variable
 const port = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+// Give permissions to get static files
+app.use(express.static("public"));
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${poet}`);
+app.get("/", function(req, res) {
+   res.sendFile(__dirname + "/public/index.html");
 });
 
+server.listen(port, () => {
+   console.log(`Listening on port: ${port}`);
+});
+
+// socket stuff
+io.sockets.on("connection", function(socket) {
+   connections.push(socket);
+   console.log(`New connections: ${connections.length} sockets connected`);
+  
+
+   // Disconnect
+   socket.on("disconnect", function(data) {
+      connections.splice(connections.indexOf(socket), 1);
+      console.log(`Socket disconnected: ${connections.length} still connected`);
+   });
+
+   // Send message
+   socket.on("send message", function(data) {
+        io.sockets.emit('new message', {msg: data});
+   });
+});
