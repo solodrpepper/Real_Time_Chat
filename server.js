79817761440bc973f385b72f916653c519463fc4
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-03-26 10:01:55 -0600

    Can send messages now

--- server.js
+++ server.js
@@ -35,16 +35,31 @@ server.listen(port, () => {
 io.sockets.on("connection", function(socket) {
    connections.push(socket);
    console.log(`New connections: ${connections.length} sockets connected`);
-  
 
    // Disconnect
    socket.on("disconnect", function(data) {
+      //if (!socket.username) return;
+      users.splice(users.indexOf(socket.username), 1);
+      updateUsernames();
       connections.splice(connections.indexOf(socket), 1);
       console.log(`Socket disconnected: ${connections.length} still connected`);
    });
 
    // Send message
    socket.on("send message", function(data) {
-        io.sockets.emit('new message', {msg: data});
+      io.sockets.emit("new message", { msg: data });
    });
+
+   // New user
+   socket.on('new user', function(data, callback) {
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+
+      updateUsernames();
+   });
+
+   function updateUsernames() {
+      io.sockets.emit('get users', users);
+   }
 });
