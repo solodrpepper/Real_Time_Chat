2bfefbfd78aba9aff00bc5694f74aaa16bafa022
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-03-28 18:50:14 -0600

    Added user creation and login

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 const express = require("express");
 const app = express();
 
+// for bycrpt
+const bcrypt = require("bcrypt");
+
 // for pooling
-const {Pool} = require('pg');
+const { Pool } = require("pg");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // set up server
 const server = require("http").createServer(app);
@@ -33,11 +36,9 @@ app.get("/", function(req, res) {
 });
 
 // For registration
-app.post('/register', handleRegister);
+app.post("/register", handleRegister);
 
-
-
-
+app.post("/login", handleLogin);
 
 server.listen(port, () => {
    console.log(`Listening on port: ${port}`);
@@ -63,7 +64,7 @@ io.sockets.on("connection", function(socket) {
    });
 
    // New user
-   socket.on('new user', function(data, callback) {
+   socket.on("new user", function(data, callback) {
       callback(true);
       socket.username = data;
       users.push(socket.username);
@@ -72,11 +73,9 @@ io.sockets.on("connection", function(socket) {
    });
 
    function updateUsernames() {
-      io.sockets.emit('get users', users);
+      io.sockets.emit("get users", users);
    }
 });
-
-
 
 //////////////////////////// In different files
 ///////////////////// controller.js
@@ -85,28 +84,88 @@ function handleRegister(req, res) {
    const password = req.body.password;
 
    createUser(username, password, function(err, data) {
-      res.redirect('index.html');   
+      if (err) {
+         return console.log(
+            `Encountered and error while creating a user: ${err}`
+         );
+      }
+      res.redirect("login.html");
    });
 }
 
+function handleLogin(req, res) {
+   const username = req.body.username;
+   const password = req.body.password;
+
+   loginUser(username, password, function(err, hash) {
+      if (err) {
+         console.log(`Encountered and error while logging in: ${err}`);
+      }
+      // compare password with stored hash
+      bcrypt.compare(password, hash, function(err, res) {
+         if (err) {
+            console.log(`There was an error verifying user: ${err}`);
+         } else if (res) {
+            // Passwords match
+            
+            // TODO: We should start a session here 
+
+            // And we finally redirect them to the home page
+            res.redirect("index.html");
+         } else {
+            // Passwords don't match
+
+            // TODO: Handle the mismatch
+         }
+      });
+   });
+}
 
 ///////////////////// model.js
 // set connection string for pooling
 const connectionString = process.env.DATABASE_URL;
-const myPool = new Pool({connectionString: connectionString});
+const myPool = new Pool({ connectionString: connectionString });
 
 // Create a new user by connecting to the database
 function createUser(username, password, callback) {
-   const sql    = "INSERT INTO users (username, hash, active_status) VALUES ($1, $2)";
-   const params = [username, password];
+   const sql =
+      "INSERT INTO users (username, hash, active_status) VALUES ($1, $2, 0)";
+
+   bcrypt.genSalt(12, function(err, salt) {
+      if (err)
+         console.log(`There was an error creating the account hash: ${err}`); //handle error
+
+      bcrypt.hash(password, salt, function(err, hash) {
+         if (err) console.log(`There was an error creating the hash: ${err}`); //handle error
+
+         // Store hash in your password DB.
+         const params = [username, hash];
+
+         myPool.query(sql, params, function(err, result) {
+            if (err) {
+               console.log(`An error occured in the DB while creating user`);
+               console.log(err);
+               callback(err, null);
+            }
+            console.log(`DB Query Finished`);
+            callback(null, result.rows);
+         });
+      });
+   });
+}
+
+// This methos will log in the user by verifying their password
+function loginUser(username, password, callback) {
+   const sql = "SELECT hash FROM users u WHERE u.username = $1";
+   const params = [username];
 
    myPool.query(sql, params, function(err, result) {
       if (err) {
-         console.log(`An error occured in the DB`)
+         console.log(`An error occured in the DB while logging in`);
          console.log(err);
          callback(err, null);
       }
       console.log(`DB Query Finished`);
       callback(null, result.rows);
-   })
+   });
 }
